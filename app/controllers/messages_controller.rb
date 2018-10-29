c6b382404915c304be196ef73d2eaa4be7ff30f3
EC2 Default User <ec2-user@ip-172-31-23-139.us-east-2.compute.internal>
2018-10-29 07:51:55 +0000

    add age column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,8 +31,8 @@ class MessagesController < ApplicationController
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
         format.json { render :show, status: :created, location: @message }
       else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+   format.html { render :new }
+   format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
